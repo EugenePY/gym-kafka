5f403453d23015917c315328c4952cbed03d3ac3
constant-c <cronrath@chalmers.se>
2018-06-27 10:11:01 +0200

    outline TODOs

--- gym_kafka/envs/kafka_env.py
+++ gym_kafka/envs/kafka_env.py
@@ -1,5 +1,5 @@
 import gym
-from gym import error, spaces, utils
+from gym import error, spaces, utils, logger
 from gym.utils import seeding
 
 
@@ -7,13 +7,32 @@ class KafkaEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self):
-        ...
+        # TODO read Kafka config from file
+        # TODO initialize action and observation space
+        # TODO initialize Kafka communications
+        self.action_space = None
+        self.observation_space = None
+
+        self.seed()
+        self.reset()
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
 
     def step(self, action):
+        assert self.action_space.contains(action)
+        self.time += 1
+        # TODO send action
+        # TODO wait for new observation & reward
+        # TODO return (obs, reward, done, info)
+
         ...
 
     def reset(self):
+        # TODO send reset command and wait for acknowledgement
         ...
 
     def render(self, mode='human', close=False):
+        # TODO think about best way to render the observation (e.g. print message?)
         ...
